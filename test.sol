ad0b2f6ea2ad0188a61e0fc1871680c3a1d4fbd9
LukaM5 <136579568+LukaM5@users.noreply.github.com>
2023-12-19 09:37:05 +0100

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,9 +1,38 @@
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
-contract Test {
-    uint a = 5;
-    memory string x = "1";
-++++
-  
-  
+contract StringConcatenation {
+    string public concatenatedString;
+
+    function concatenateAndAssign(uint256 number, string memory additionalString) public {
+        string memory numberAsString = toString(number);
+        concatenatedString = concatenateStrings(numberAsString, additionalString);
+    }
+
+    function toString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) {
+            return "0";
+        }
+
+        uint256 temp = value;
+        uint256 length;
+        
+        while (temp > 0) {
+            length++;
+            temp /= 10;
+        }
+
+        bytes memory result = new bytes(length);
+        temp = value;
+        
+        while (temp > 0) {
+            result[--length] = bytes1(uint8(48 + temp % 10));
+            temp /= 10;
+        }
+
+        return string(result);
+    }
+
+    function concatenateStrings(string memory a, string memory b) internal pure returns (string memory) {
+        return string(abi.encodePacked(a, b));
+    }
 }
